3271003e6b566fcfd66086a278a841445cbe2d26
Haider Khan <haiderkhan.live@gmail.com>
2022-08-06 14:58:08 +0500

    fixed sequalize modal imports

--- app/templates/sequelize/models/index.js
+++ app/templates/sequelize/models/index.js
@@ -1,45 +1,55 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV;
 const config = require(`${global.paths.config}/database`)[env];
 let db = {};
-const { startCase } = require('lodash');
+const { startCase } = require("lodash");
 
 let sequelize;
 if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
 function getNormalizedNameOfModel(modelName) {
-    return startCase(modelName).replace(/\s/g, '');
+  return startCase(modelName).replace(/\s/g, "");
 }
 
 fs.readdirSync(__dirname)
-    .filter(file => {
-        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-    })
-    .forEach(file => {
-        const model = sequelize['import'](path.join(__dirname, file));
-        const normalizedName = getNormalizedNameOfModel(model.name);
-        db[normalizedName] = model;
-    });
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    const normalizedName = getNormalizedNameOfModel(model.name);
+    db[normalizedName] = model;
+  });
 
-Object.keys(db).forEach(modelName => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
 db = {
-    ...db,
-    sequelize: sequelize,
-    Sequelize: Sequelize,
+  ...db,
+  sequelize: sequelize,
+  Sequelize: Sequelize,
 };
 
 module.exports = db;
